ad20a769b7fe6931bf863d4a17092cf68db82c3a
Neelakshi <neelakshikadyan@gmail.com>
2026-03-19 17:42:45 +0530

    fix: allow all vercel subdomains via cors regex

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,25 @@ async function bootstrap() {
 
   // Enable CORS so your Angular app (localhost:4200) can call this API
   app.enableCors({
-    origin: [
+    origin: (origin, callback) => {
+    const allowed = [
       'http://localhost:4200',       // Angular dev server
       'https://portfolio-backend-yltt.onrender.com'  // Replace with your deployed URL later
       ,'https://portfolio-gules-psi-64.vercel.app/',
           'https://portfolio-n00ocl1jn-neelakshis-projects.vercel.app',
+      /\.vercel\.app$/,      // Allow all .vercel
 
-    ],
+    ]
+    if (!origin) return callback(null, true); // allow server-to-server
+
+    const isAllowed = allowed.some(pattern =>
+      typeof pattern === 'string'
+        ? pattern === origin
+        : pattern.test(origin)
+    );
+
+    callback(isAllowed ? null : new Error('Not allowed by CORS'), isAllowed);
+  },
     methods: ['GET', 'POST'],
     credentials: true,
   });
